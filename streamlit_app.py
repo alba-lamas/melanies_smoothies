922c7f1062fde0241c48a714719ffca5f12ec5e4
Alba Lamas <alba.v.lamas@gmail.com>
2024-09-05 15:31:01 +0000

    xd

--- streamlit_app.py
+++ streamlit_app.py
@@ -46,7 +46,7 @@ if ingredients_list:
         st.subheader(fruit_chosen + ' Nutrition Information')
         
         fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + fruit_chosen)
-        fv_df = st.dataframe(data= fruityvice_response.json()['nutritions'], use_container_width=True)
+        fv_df = st.dataframe(data= fruityvice_response.json(), use_container_width=True)
     ingredients_string = ingredients_string.strip(', ')
     st.write(ingredients_string)
     
